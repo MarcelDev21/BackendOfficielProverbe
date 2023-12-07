ada4e91915285113c7b3a34fc131d458a0fb005a
MarcelDev <marceldev96@gmail.com>
2023-12-07 22:19:30 +0100

    ajout du port

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port = 3000
 dotenv.config()
 mongoose.connect(process.env.MongoUrl).then((response)=> console.log("Connecté avec succès")).catch((error)=> console.log("error"))
 
+app.use(express.json({limit: '100mb'}))
+app.use(express.urlencoded({limit: '100mb', extended: true}))
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
